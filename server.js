a48406b45102ebb9402cf04d3353fd2db876ec51
DennisBinford <dennis@binford.org>
2023-08-12 14:13:38 -0500

    added port environment variable

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 
+const port = process.env.port || 3000
 const connectDB = require('./db.js')
 const sectionRouter = require('./routes/sectionroute')
 const professorRouter = require('./routes/professorroute')
